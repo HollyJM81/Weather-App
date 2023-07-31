9c1adc285ed52cedad6bb1b7aefb0f8836450ce4
Holly Mathieson <hjmathieson@gmail.com>
2023-07-31 22:24:32 +0100

    import weather json

---
+++ src/components/LocationDetails.js
@@ -0,0 +1,6 @@
+// create a component called <LocationDetails />, which uses JSX
+// to render values stored in props.city and props.country inside of
+// a <h1> tag. Give your <h1> tag an appropriate class name.
+// Export the <LocationDetails /> component from the file.
+
+<LocationDetails />;
