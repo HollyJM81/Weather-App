8db8a4cd72e54b7d9770ee972a07228933b4329e
Holly Mathieson <hjmathieson@gmail.com>
2023-08-15 01:35:56 +0100

    weather icons rendering correctly

--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -1,9 +1,7 @@
 import React from "react";
 import moment from "moment";
 
-import { weather } from "../data/iconData.json";
-console.log(weather);
-
+import iconData from "../data/iconData.json";
 let weatherId;
 
 function ForecastSummary(props) {
@@ -15,25 +13,14 @@ function ForecastSummary(props) {
 			weatherId = icon.slice(0, 1) + "00";
 		}
 	};
-	weatherCode(icon);
-	console.log(weatherId);
-	// still not locating the icon properly... we need to grab the icon that corresponds to
-	// the id matching the updated weatherId;
-	let filtered = weather.filter((row) => row.id);
-	console.log(filtered);
-
-	// the above gives us an unchanged array
-
-	// make dummyUrl = the value of the "icon" key, matching the id value.
-	const dummyURL = `https://openweathermap.org/img/wn/10d@2x.png`;
-
+	weatherCode();
 	return (
 		<div className="forecast-summary" data-testid="forecast-summary">
 			<div className="forecast-summary__date">
 				{moment(date).format("ddd Do MMM")}
 			</div>
 			<div className="forecast-summary__icon" data-testid="forecast-icon">
-				<img alt="weather icon" src={dummyURL} />
+				<img alt="weather icon" src={iconData[weatherId]} />
 			</div>
 			<div className="forecast-summary__temperature">
 				{temperature.max}&deg;C
